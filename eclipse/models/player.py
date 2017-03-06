c8b3cceb83f4133ae978806703a1f6d7659469b5
Mike <odingrey@gmail.com>
2017-03-05 18:54:02 -0800

    Added some logic on the player save since it's a bit complex.  Basically throws an error if it's saving an incorrectly formatted Player

--- eclipse/models/player.py
+++ eclipse/models/player.py
@@ -32,6 +32,23 @@ class Player(models.Model):
 		on_delete=models.DO_NOTHING
 	)
 
+	def save(self, *args, **kwargs):
+		# Error if more than one is set.
+		if sum([1 for x in (self.npc, self.user, self.government,) if x]) > 1:
+			raise AttributeError('Requires User, NPC or Government, not more than one')
+
+		# Error if none are set.
+		if sum([1 for x in (self.npc, self.user, self.government,) if x]) == 0:
+			raise AttributeError('Requires User, NPC or Government')
+
+		# Error if location is incorrect.
+		if self.npc or self.user and not self.location:
+			raise AttributeError('Requires location to be set if player is NPC or User')
+		if self.government:
+			self.location = None
+		
+		super(Player, self).save(*args, **kwargs)
+
 	def __unicode__(self):
 		if self.user:
 			return "User: " + self.user.username
