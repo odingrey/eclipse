77c76103a2a8164ad52ceb8d9d12b8fb71b7d0e2
Mike <odingrey@gmail.com>
2017-03-12 12:28:40 -0700

    Added more tests for management, fixed issue where apparently I didn't merge a fix from master...

--- eclipse/tests/test_management.py
+++ eclipse/tests/test_management.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from eclipse.models.location import Location
 from eclipse.models.ship import Ship
 from eclipse.models.shipclass import ShipClass
+from eclipse.models.solarsystem import SolarSystem
 from eclipse.models.station import Station
 
 class MoveTest(TestCase):
@@ -33,3 +34,53 @@ class MoveTest(TestCase):
 		self.assertNotEqual(ship.location.y, original_loc.y)
 		self.assertNotEqual(ship.location.z, original_loc.z)
 		self.assertEqual(ship.location.solar_system, original_loc.solar_system)
+
+	def testMoveCorrectlyOutwards(self):
+		sol = SolarSystem.objects.get(pk='Sol')
+		location2 = Location(x=200,y=200,z=200,solar_system=sol)
+		ship = Ship.objects.get(pk=1)
+		ship.location.x=100
+		ship.location.y=100
+		ship.location.z=100
+		ship.location.save()
+		ship.save()
+		ship.move(location2)
+
+		expected = {
+			'x': 128.8675134594813,
+			'y': 128.8675134594813,
+			'z': 128.8675134594813,
+		}
+
+		call_command('tick')
+		# Grab ship from DB again
+		ship = Ship.objects.get(pk=1)
+
+		self.assertEqual(ship.location.x, expected['x'])
+		self.assertEqual(ship.location.y, expected['y'])
+		self.assertEqual(ship.location.z, expected['z'])
+
+	def testMoveCorrectlyInwards(self):
+		sol = SolarSystem.objects.get(pk='Sol')
+		location2 = Location(x=100,y=100,z=100,solar_system=sol)
+		ship = Ship.objects.get(pk=1)
+		ship.location.x=200
+		ship.location.y=200
+		ship.location.z=200
+		ship.location.save()
+		ship.save()
+		ship.move(location2)
+
+		expected = {
+			'x': 171.1324865405187,
+			'y': 171.1324865405187,
+			'z': 171.1324865405187,
+		}
+
+		call_command('tick')
+		# Grab ship from DB again
+		ship = Ship.objects.get(pk=1)
+
+		self.assertEqual(ship.location.x, expected['x'])
+		self.assertEqual(ship.location.y, expected['y'])
+		self.assertEqual(ship.location.z, expected['z'])
